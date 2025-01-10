16e56852050fa5d8b070767f7d7d183297599350
bakabakashiii <159062438+bakabakashiii@users.noreply.github.com>
2025-01-10 20:17:12 +0300

    Update 916. Word Subsets.py

--- python/916. Word Subsets.py
+++ python/916. Word Subsets.py
@@ -1,1 +1,26 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        counts = {}
 
+        for word in words2:
+            letters = Counter(word)
+            for a, b in letters.items():
+                if a in counts:
+                    counts[a] = max(counts[a], b)
+                else:
+                    counts[a] = b
+
+        ans = []
+        for word in words1:
+            letters = Counter(word)
+            isValid = True
+
+            for a in counts:
+                if letters.get(a, 0) < counts[a]:
+                    isValid = False
+                    break
+                    
+            if isValid:
+                ans.append(word)
+                    
+        return ans
