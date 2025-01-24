517c3df4821a73901f78d2418bd25af036ef9621
bakabakashiii <159062438+bakabakashiii@users.noreply.github.com>
2025-01-24 19:03:36 +0300

    Create 802. Find Eventual Safe States.py

---
+++ python/802. Find Eventual Safe States.py
@@ -0,0 +1,20 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        safe = {}
+
+        def dfs(i):
+            if i in safe:
+                return safe[i]
+            safe[i] = False
+            for nei in graph[i]:
+                if not dfs(nei):
+                    return False
+            safe[i] = True
+            return True
+            
+        ans = []
+        for i in range(n):
+            if dfs(i):
+                ans.append(i)
+        return ans
